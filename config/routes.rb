e9ffc1727493ff4bf42a45005abcdfab12c900d3
David Klaver <davidjklaver@gmail.com>
2017-05-03 16:11:35 -0400

    various styling, including menu bar color for dishes controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 	get '/' => 'pages#index'
 
 	get '/dishes' => 'dishes#index'
-	get '/dish/:id' => 'dishes#show'
+	# get '/dish/:id' => 'dishes#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
