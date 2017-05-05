279e1f40ddae4ecd2f02f4aab7a9271b0b37fb5b
David Klaver <davidjklaver@gmail.com>
2017-05-05 08:00:44 -0400

    make font-awesome source the cdn

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,29 @@
 class DishesController < ApplicationController
+	before_filter :set_current_page
+	def set_current_page
+	  @current_page = "dishes"
+	end
 	def index
 		@dishes = Dish.all
-		@current_page = "dishes"
+		@sundaes = Dish.where(category: "Sundaes")
+	end
+
+	def show
+		@dish = Dish.find(params[:id])
+	end
+
+	def new
+		@dish = Dish.new
+	end
+
+	def create
+		@dish = Dish.new(name: params[:name], description: params[:description], price: params[:price], category: params[:category])
+		
+		if @dish.save
+			# flash[:success] = "You added a new product!"
+			redirect_to "/dishes/#{@dish.id}"
+		else
+			render 'new.html.erb'
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	get '/' => 'pages#index'
 
 	get '/dishes' => 'dishes#index'
-	# get '/dish/:id' => 'dishes#show'
+	get 'dishes/new' => 'dishes#new'
+	post '/dishes' => 'dishes#create'
+	get '/dishes/:id' => 'dishes#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
