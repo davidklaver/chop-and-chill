1a54a2bdeff565eebaf31acdf9ed33a945d8adc4
David Klaver <davidjklaver@gmail.com>
2017-05-27 23:14:04 -0400

    various styling

--- config/initializers/dynopoker.rb
+++ config/initializers/dynopoker.rb
@@ -1,5 +1,5 @@
 Dynopoker.configure do |config|
   config.address = 'http://wakemydyno.com'
 #  config.enable = false # default is true
-#  config.poke_frequency = 123 # default is 1800s (30min)
+  config.poke_frequency = 180 # default is 1800s (30min)
 end
