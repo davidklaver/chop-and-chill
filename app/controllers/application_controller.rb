6e095ef62a1263250a90f379afc6886bc5b36baf
David Klaver <davidjklaver@gmail.com>
2017-06-21 16:34:37 -0400

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def cart_count
     # User.where(["name = ? and email = ?", "Joe", "joe@example.com"])
 # SELECT * FROM users WHERE name = 'Joe' AND email = 'joe@example.com';
-  	@cart_count = CartedDish.where("status = ? and session_id = ?", "carted", session.id).count
+  	@cart_count = session[:cart].count
   end
   helper_method :cart_count
 
