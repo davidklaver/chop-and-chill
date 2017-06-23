dd5a2570c9af41b63019e72366fe4ab7ed6d1a63
David Klaver <davidjklaver@gmail.com>
2017-06-23 05:09:08 -0400

    wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -90,8 +90,8 @@ class OrdersController < ApplicationController
 
     RestClient.post "https://api:key-363879ea1d06f74b44d685f4484f33ec"\
     "@api.mailgun.net/v3/sandbox43c98faad09044ccb5cf61efc5442aa8.mailgun.org/messages",
-    :from => "Chop and Chill <chop-and-chill.herokuapp.com>",
-    # :from => "Mailgun Sandbox <postmaster@sandbox43c98faad09044ccb5cf61efc5442aa8.mailgun.org>",
+    # :from => "Chop and Chill <chop-and-chill.herokuapp.com>",
+    :from => "Mailgun Sandbox <postmaster@sandbox43c98faad09044ccb5cf61efc5442aa8.mailgun.org>",
     :to => "David Klaver <davidjklaver@gmail.com>",
     :subject => "Congrats on your Chop and Chill Order!",
     :text => "Here's your order info: you ordered blah blah blah"
