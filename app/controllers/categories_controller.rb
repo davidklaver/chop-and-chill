edfb615bdbac01a869a67e7fe3e77596f1f98474
David Klaver <davidjklaver@gmail.com>
2017-06-25 09:30:17 -0400

    attempt to fix cart error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,10 @@ class CategoriesController < ApplicationController
 	def index
 		@current_page = "categories"
 		@categories = Category.all
+		p "*" * 50
+		p "here's sessioncart in categories:"
+		p session[:cart]
+		p "*" * 50
 	end
 
 	def show
